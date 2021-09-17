4bbd2ab055995e661879063451235aff4bebb22a
Nancy Soontiens <nancy.soontiens@dfo-mpo.gc.ca>
2021-09-17 21:23:57 -0230

    some scripts for plotting and computing errors

---
+++ scripts/errors.py
@@ -0,0 +1,106 @@
+"""
+Module to calculate velocity errors
+Nancy Soontiens
+"""
+
+import numpy as np
+import xarray as xr
+
+
+def process_drifter(d):
+    """Add the following variables to this drifter dataset:
+    speed_drifter
+    speed_ocean
+    [speed_atmos]
+    bearing_drifter
+    bearing_ocean
+    [bearing_atmos]
+    [ueast_adjusted]
+    [vnorth_adjusted]
+    [speed_adjusted]
+    [bearing_adjusted]
+    Variables in square brackets are only added if winds were used in 
+    correction calculations.
+
+    Errors and vector differences are also added.
+    """
+
+    if 'alpha_real' in d.variables:
+        adjusted = calculated_adjusted_bearing(d)
+        d['ueast_adjusted'] = xr.DataArray(adjusted.real, dims=('time'))
+        d['vnorth_adjusted'] = xr.DataArray(adjusted.imag, dims=('time'))
+    d = append_speed_and_bearing(d)
+    d = append_errors(d)
+    return d
+
+
+def calculate_adjusted_current(d):
+    """Calculate an adjusted current adjusted = ocean + mean(alpha)*atmos"""
+    vector_ocean = []
+    vector_atmos = []
+    mean_alpha = complex(d['alpha_real'].mean(), d['alpha_imag'].mean())
+    n = len(d['ueast_ocean'])
+    for i in range(n):
+        vector_ocean.append(complex(d['ueast_ocean'][i], d['vnorth_ocean'][i]))
+        vector_atmos.append(complex(d['ueast_atmos'][i], d['vnorth_atmos'][i]))
+    adjusted = np.array(vector_ocean) + mean_alpha * np.array(vector_atmos)
+    return adjusted
+
+        
+def append_speed_and_bearing(d):
+    """Add speed and bearing to dataset"""
+    types = ['ocean', 'drifter']
+    if 'ueast_atmos' in d.variables:
+        types.append('atmos')
+    if 'ueast_adjusted' in d.variables:
+        types.append('adjusted')
+    for t in types:
+        d['speed_{}'.format(t)] = magnitude(d['ueast_{}'.format(t)],
+                                            d['vnorth_{}'.format(t)])
+        bearing = calculate_bearing(d['ueast_{}'.format(t)],
+                                    d['vnorth_{}'.format(t)])
+        d['bearing_{}'.format(t)] = xr.DataArray(bearing, dims=('time'))
+    return d
+
+          
+def append_errors(d):
+    """Append errors for the drifter dataset. The following errors are calculated:
+    speed_ocean_error
+    bearing_ocean_error
+    [speed_adjusted_error]
+    [bearing_adjusted_error]
+    ueast_ocean_vdiff
+    vnorth_ocean_vdiff
+    [ueast_adjusted_vdiff]
+    [vnorth_adjusted_vdiff]
+    Items in brackets are only added if winds were used in correction computations.
+    """
+    types = ['ocean',]
+    if 'bearing_adjusted' in d.variables:
+        types.append('adjusted')
+    for t in types:
+        # Bearing error
+        diff = d['bearing_{}'.format(t)] - d['bearing_drifter']
+        diff[diff>180] = diff[diff>180]-360
+        diff[diff<-180] = diff[diff<-180]+360
+        d['bearing_{}_error'.format(t)] = diff
+        # Speed error
+        d['speed_{}_error'.format(t)] = d['speed_{}'.format(t)] - d['speed_drifter']
+        # Vector difference
+        d['ueast_{}_vdiff'.format(t)] = d['ueast_{}'.format(t)] - d['ueast_drifter']
+        d['vnorth_{}_vdiff'.format(t)] = d['vnorth_{}'.format(t)] - d['vnorth_drifter']
+    return d
+
+
+def calculate_bearing(east, north):
+    """Calculate the bearing of a vector"""
+    radians = np.arctan2(east, north)
+    degrees = radians*180./np.pi
+    degrees[degrees<0] = degrees[degrees<0] +360
+    return degrees
+
+
+def magnitude(x, y):
+    """Calculate the magnitude of a vector"""
+    return np.sqrt(x**2 + y**2)
+
---
+++ scripts/plot_bioSVPs.py
@@ -0,0 +1,68 @@
+"""Script to plot maps from biosSVPs-CIOPSEv2"""
+
+import glob
+import os
+import pickle
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import errors
+
+src_dir='../data/bioSVPs-CIOPSEv2/'
+pickle_file = os.path.join(src_dir, 'all_bioSVPs-CIOPSEv2.pickle')
+
+
+def main():
+    # Load data
+    if not os.path.exists(pickle_file):
+        dfall = load_data(src_dir)
+        with open(pickle_file, 'wb') as f:
+            pickle.dump(dfall, f)
+    else:
+        with open(pickle_file, 'rb') as f:
+            dfall = pickle.load(f)
+    fig, ax = plt.subplots(1, 1, figsize=(15,5))
+    mesh = plot_error_map(dfall, ax)
+    cbar = plt.colorbar(mesh, ax=ax)
+    cbar.set_label('ocean speed error [m/s]')
+    plt.show()
+
+    
+def plot_error_map(df, ax, error='speed_ocean_error',
+                   lon_min=-72, lat_min=37, lon_max=-36, lat_max=46,
+                   cmap='RdBu_r', vmin=-1, vmax=1, latstep=2, lonstep=5):
+    m =  Basemap(llcrnrlon=lon_min, llcrnrlat=lat_min,
+                 urcrnrlon=lon_max, urcrnrlat=lat_max,
+                 projection='merc', resolution='h', ax=ax)
+    x, y = m(df.lon.values, df.lat.values)
+    mesh = m.hexbin(x, y, C=df[error].values, cmap=cmap, vmin=vmin, vmax=vmax)
+    m.drawcoastlines()
+    m.drawmeridians(np.arange(lon_min, lon_max, lonstep), labels=[False, False, False, True])
+    m.drawparallels(np.arange(lat_min, lat_max, latstep), labels=[True, False, False, False])
+    return mesh
+
+            
+def load_data(src_dir):
+    files = glob.glob(os.path.join(src_dir, '*.nc'))
+    files.sort()
+    dfall = pd.DataFrame()
+    for f in files:
+        d = xr.open_dataset(f)
+        d = errors.process_drifter(d)
+        df = d.to_dataframe()
+        df['buoyid'] = d.attrs['obs_buoyid']
+        if 'obs_drifter_type' in d.attrs:
+            df['drifter_type'] = d.attrs['obs_drifter_type']
+        elif 'obs_model' in d.attrs:
+            df['drifter_type'] = d.attrs['obs_model']
+        dfall = pd.concat([dfall,df])
+    return dfall
+
+
+
+if __name__ == '__main__':
+    main()
