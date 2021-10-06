118cae682bf4ecea0c623533567206db9be7d972
Nancy Soontiens <nancy.soontiens@dfo-mpo.gc.ca>
2021-10-06 12:44:29 -0230

    plotting 2021 TREX oskers

--- scripts/plot_TREX2021.py
+++ scripts/plot_TREX2021.py
@@ -12,36 +12,47 @@ import xarray as xr
 
 import errors
 
-src_dir='../data/TREX-GSL500-relocatable1km'
-pickle_file = os.path.join(src_dir, 'all_TREX-GSL500-relocatable1km.pickle')
+src_dirs = {'ciopse-par': '../data/TREX2021oskers-CIOPSEv2-mshydro',
+            'ciopse-ops': '../data/TREX2021oskers-CIOPSEv1p5-mshydro'}
+pickle_files = {
+    exp: os.path.join(src_dirs[exp],
+                      'all_{}.pickle'.format(os.path.basename(src_dirs[exp])))
+    for exp in src_dirs.keys()
+}
 
 
 def main():
     # Load data
-    if not os.path.exists(pickle_file):
-        dfall = load_data(src_dir)
-        with open(pickle_file, 'wb') as f:
-            pickle.dump(dfall, f)
-    else:
-        with open(pickle_file, 'rb') as f:
-            dfall = pickle.load(f)
-    print(dfall.mean())
-    print(dfall.std())
-    fig, ax = plt.subplots(1, 1, figsize=(15,5))
-    mesh = plot_error_map(dfall, ax, error='speed_adjusted_error')
-    cbar = plt.colorbar(mesh, ax=ax)
-    cbar.set_label('adjusted speed - drifter speed [m/s]')
-    plt.show()
+    for exp in src_dirs.keys():
+        src_dir = src_dirs[exp]
+        pickle_file = pickle_files[exp]
+        if not os.path.exists(pickle_file):
+            dfall = load_data(src_dir)
+            with open(pickle_file, 'wb') as f:
+                pickle.dump(dfall, f)
+        else:
+            with open(pickle_file, 'rb') as f:
+                dfall = pickle.load(f)
+        stats = dfall.agg(['mean', 'std', 'var']).transpose()
+        stats.to_csv('{}.txt'.format(os.path.basename(src_dir)))
+        fig, ax = plt.subplots(1, 1, figsize=(15,5))
+        mesh = plot_error_map(dfall, ax, error='speed_adjusted_error')
+        cbar = plt.colorbar(mesh, ax=ax)
+        cbar.set_label('adjusted speed - drifter speed [m/s]')
+        ax.set_title(exp)
+        fig.savefig('{}-adjusted_error.png'.format(os.path.basename(src_dir)),
+                    bbox_inches='tight')
 
     
 def plot_error_map(df, ax, error='speed_ocean_error',
-                   lon_min=-69, lat_min=48, lon_max=-61, lat_max=51,
+                   lon_min=-69, lat_min=48, lon_max=-61, lat_max=50,
                    cmap='RdBu_r', vmin=-1, vmax=1, latstep=1, lonstep=1):
     m =  Basemap(llcrnrlon=lon_min, llcrnrlat=lat_min,
                  urcrnrlon=lon_max, urcrnrlat=lat_max,
                  projection='merc', resolution='h', ax=ax)
     x, y = m(df.lon.values, df.lat.values)
-    mesh = m.hexbin(x, y, C=df[error].values, cmap=cmap, vmin=vmin, vmax=vmax)
+    mesh = m.hexbin(x, y, C=df[error].values, cmap=cmap, vmin=vmin, vmax=vmax,
+                    gridsize=40)
     m.drawcoastlines()
     m.drawmeridians(np.arange(lon_min, lon_max, lonstep), labels=[False, False, False, True])
     m.drawparallels(np.arange(lat_min, lat_max, latstep), labels=[True, False, False, False])
